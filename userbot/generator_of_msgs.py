96073618cf0398526ad7ce2e56539ab03ffc19fc
Loen15 <gapoanivan@gmail.com>
2024-02-14 18:17:37 +0300

    Теперь систем промпт добавляется за 50 сообщений до конца диалога

--- userbot/generator_of_msgs.py
+++ userbot/generator_of_msgs.py
@@ -5,13 +5,14 @@ from pyrogram import Client
 # функция генерирующая чат для GPT
 def generate_chat(app: Client, text: str, chat_id: int, prompt_for_ai: str, is_user: bool):
   # формируем список json обьектов для GPT
-  msgs = [{"role": "system","content": prompt_for_ai}]
-  msgs.append({"role": "user" if is_user else "assistant","content": text})
+  #msgs = [{"role": "system","content": prompt_for_ai}]
+  msgs = [{"role": "user" if is_user else "assistant","content": text}]
   for msg in app.get_chat_history(chat_id, offset = 1):
     if msg.text ==  None and msg.caption == None: continue
     text = msg.text if msg.text !=  None else msg.caption
     if msg.from_user.is_self:
-      msgs.insert(1,{"role": "assistant","content": text})
+      msgs.insert(0,{"role": "assistant","content": text})
     else:
-      msgs.insert(1,{"role": "user","content": text})
+      msgs.insert(0,{"role": "user","content": text})
+  msgs.insert(-50,{"role": "system","content": prompt_for_ai})
   return msgs
