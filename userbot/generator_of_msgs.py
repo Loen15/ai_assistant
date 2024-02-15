8b97ea2f49edf27bb16581a107836457c7692f9b
Loen15 <gapoanivan@gmail.com>
2024-02-15 16:16:20 +0300

    Убрал откоменченный мусор

--- userbot/generator_of_msgs.py
+++ userbot/generator_of_msgs.py
@@ -5,7 +5,6 @@ from pyrogram import Client
 # функция генерирующая чат для GPT
 def generate_chat(app: Client, text: str, chat_id: int, prompt_for_ai: str, is_user: bool):
   # формируем список json обьектов для GPT
-  #msgs = [{"role": "system","content": prompt_for_ai}]
   msgs = [{"role": "user" if is_user else "assistant","content": text}]
   for msg in app.get_chat_history(chat_id, offset = 1):
     if msg.text ==  None and msg.caption == None: continue
